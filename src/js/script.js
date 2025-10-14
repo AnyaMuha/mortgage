14fcd7e60d2840c193afceabb92c0852cf9e1a0a
ana <anyamakh.dev@gmail.com>
2025-10-14 16:36:42 -0300

    Header/Hero: split sections, applied feedback, added mobile nav scroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,18 @@
 import "/src/sass/style.scss";
 
 // burger
-const burger = document.querySelector(".hero__burger");
-const close = document.querySelector(".hero__menu-close");
-const menu = document.querySelector(".hero__menu");
-const overlay = document.querySelector(".hero__fill");
+const burger = document.querySelector(".header__burger");
+const close = document.querySelector(".header__menu-close");
+const menu = document.querySelector(".header__menu");
+const overlay = document.querySelector(".header__fill");
 burger?.addEventListener("click", () => {
-  menu?.classList.add("hero__menu_active");
+  menu?.classList.add("header__menu_active");
   document.body.style.overflow = "hidden";
 });
 close?.addEventListener("click", () => {
-  menu?.classList.remove("hero__menu_active");
+  menu?.classList.remove("header__menu_active");
   document.body.style.overflow = "";
 });
 overlay.addEventListener("click", () => {
-  menu.classList.remove("active");
+  menu.classList.remove("header__menu_active");
 });
