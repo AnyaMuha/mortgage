e38b9d20d0902a4a3db086c787d2ddf521144c74
ana <anyamakh.dev@gmail.com>
2025-10-20 11:48:53 -0300

    Home page Getting & Journey fixed minor issues

--- src/js/script.js
+++ src/js/script.js
@@ -16,3 +16,33 @@ close?.addEventListener("click", () => {
 overlay.addEventListener("click", () => {
   menu.classList.remove("header__menu_active");
 });
+
+const dots = document.querySelectorAll(".journey__dot");
+const steps = document.querySelectorAll(".journey__step");
+
+if (dots.length && steps.length) {
+  // функция очистки активных классов
+  const clearActive = () => {
+    dots.forEach((dot) => dot.classList.remove("is-active"));
+    steps.forEach((step) => step.classList.remove("is-current"));
+  };
+
+  // клик по шагу
+  steps.forEach((step, index) => {
+    step.addEventListener("click", () => {
+      clearActive();
+      dots[index].classList.add("is-active");
+      step.classList.add("is-current");
+    });
+  });
+
+  // клик по кружку
+  dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+      clearActive();
+      dot.classList.add("is-active");
+      steps[index].classList.add("is-current");
+      steps[index].scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+  });
+}
